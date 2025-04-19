42757bcd3bea9661e40ece75b30493cbc0202cc2
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-04-20 00:28:44 +0200

    Simplify man!

--- appstore.py
+++ appstore.py
@@ -432,50 +432,48 @@ def execute_script(script_source, is_file, lvgl_obj):
     except Exception as e:
         print(f"Thread {thread_id}: error ", e)
 
+
 # Child script buffer: updates label, adds button and slider
 script_buffer = """
 import time
-def app_main():
-    print("Child coroutine: Creating UI")
-    # Label
-    label = lv.label(subwindow)
-    label.set_text("Child: 0")
-    label.set_style_text_font(lv.font_montserrat_12, 0)
-    label.align(lv.ALIGN.TOP_MID, 0, 10)
-    # Button
-    button = lv.button(subwindow)
-    button.set_size(80, 40)
-    button.align(lv.ALIGN.CENTER, 0, 0)
-    button_label = lv.label(button)
-    button_label.set_text("Quit")
-    button_label.set_style_text_font(lv.font_montserrat_12, 0)
-    # Slider
-    slider = lv.slider(subwindow)
-    slider.set_range(0, 100)
-    slider.align(lv.ALIGN.BOTTOM_MID, 0, -30)
-    # Quit flag
-    should_continue = True
-    # Button callback
-    def button_cb(e):
-        nonlocal should_continue
-        print("Quit button clicked, exiting child")
-        should_continue = False
-    button.add_event_cb(button_cb, lv.EVENT.CLICKED, None)
-    # Slider callback
-    def slider_cb(e):
-        value = slider.get_value()
-        print("Child slider value:", value)
-    slider.add_event_cb(slider_cb, lv.EVENT.VALUE_CHANGED, None)
-    # Update loop
-    count = 0
-    while should_continue:
-        count += 1
-        print("Child coroutine: Updating label to", count)
-        label.set_text(f"Child: {count}")
-        time.sleep_ms(1000)
-    print("Child coroutine: Exiting")
-
-app_main()    
+print("Child coroutine: Creating UI")
+# Label
+label = lv.label(subwindow)
+label.set_text("Child: 0")
+label.set_style_text_font(lv.font_montserrat_12, 0)
+label.align(lv.ALIGN.TOP_MID, 0, 10)
+# Button
+button = lv.button(subwindow)
+button.set_size(80, 40)
+button.align(lv.ALIGN.CENTER, 0, 0)
+button_label = lv.label(button)
+button_label.set_text("Quit")
+button_label.set_style_text_font(lv.font_montserrat_12, 0)
+# Slider
+slider = lv.slider(subwindow)
+slider.set_range(0, 100)
+slider.align(lv.ALIGN.BOTTOM_MID, 0, -30)
+# Quit flag
+should_continue = True
+# Button callback
+def button_cb(e):
+    global should_continue
+    print("Quit button clicked, exiting child")
+    should_continue = False
+button.add_event_cb(button_cb, lv.EVENT.CLICKED, None)
+# Slider callback
+def slider_cb(e):
+    value = slider.get_value()
+    print("Child slider value:", value)
+slider.add_event_cb(slider_cb, lv.EVENT.VALUE_CHANGED, None)
+# Update loop
+count = 0
+while should_continue:
+    count += 1
+    print("Child coroutine: Updating label to", count)
+    label.set_text(f"Child: {count}")
+    time.sleep_ms(1000)
+print("Child coroutine: Exiting")
 """
 
 
