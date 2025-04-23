377b49a5ffb7f2dc84ac5ce30ea22e23ec17a519
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-04-23 12:33:02 +0200

    Add pngtobin

---
+++ pngtobin.sh
@@ -0,0 +1,2 @@
+#~/sources/lvgl_micropython/lib/lvgl/scripts/LVGLImage.py --ofmt BIN --cf RGB565 -o icon_64x64.bin icon_64x64.png 
+~/sources/lvgl_micropython/lib/lvgl/scripts/LVGLImage.py --ofmt BIN --cf RGB565 icon_64x64.png 
