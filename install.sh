7e87fa4a3a96543e1ebceb81888c8cc6745c3975
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-04-23 16:28:23 +0200

    Add QMI8685 accelerometer driver

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ pkill -f "python.*mpremote"
 ~/sources/lvgl_micropython/lib/micropython/tools/mpremote/mpremote.py fs cp main.py :/main.py
 
 ~/sources/lvgl_micropython/lib/micropython/tools/mpremote/mpremote.py fs cp -r apps :/
+~/sources/lvgl_micropython/lib/micropython/tools/mpremote/mpremote.py fs cp -r lib :/
 ~/sources/lvgl_micropython/lib/micropython/tools/mpremote/mpremote.py fs cp -r resources :/
 
 ~/sources/lvgl_micropython/lib/micropython/tools/mpremote/mpremote.py reset
