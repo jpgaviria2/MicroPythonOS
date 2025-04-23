3f3433492c86f3187c979816f47c71d319a8438b
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-04-23 17:34:18 +0200

    Use qmi8658 as-is
    Taken from https://github.com/echo-lalia/qmi8658-micropython/blob/main/qmi8685.py but renamed correctly.

---
+++ lib/README.md
@@ -0,0 +1,2 @@
+This /lib folder contains:
+- https://github.com/echo-lalia/qmi8658-micropython/blob/main/qmi8685.py but given the correct name "qmi8658.py"
--- lib/qmi8658.py
+++ lib/qmi8658.py
@@ -1,7 +1,30 @@
-# A simple driver for the QMI8658 IMU.
-# From https://github.com/echo-lalia/qmi8658-micropython
-# But removed the multi line comments that were causing problems.
+"""A simple driver for the QMI8658 IMU.
 
+This driver is based on bjepson's CircuitPython driver.
+
+
+MIT License
+
+Copyright (c) 2024 Ethan Lacasse
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice (including the next
+paragraph) shall be included in all copies or substantial portions of the
+Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
 import struct
 import time
 
@@ -65,6 +88,7 @@ _ODR_62_5HZ = const(0b0111)
 
 
 class QMI8658:
+    """QMI8658 inertial measurement unit."""
 
     def __init__(
             self,
