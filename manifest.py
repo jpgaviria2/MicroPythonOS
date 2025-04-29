e4372a984c5a60c5e207c92e3fec740a70b48c82
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-04-29 20:24:05 +0200

    Launcher icons work!

--- manifest.py
+++ manifest.py
@@ -1,6 +1,4 @@
 freeze('internal_filesystem/', 'boot.py') # Hardware initialization
 freeze('internal_filesystem/', 'main.py') # User Interface initialization
-#freeze('', 'boot.py') # Hardware initialization
-#freeze('', 'main.py') # User Interface initialization
 freeze('internal_filesystem/lib', '') # Additional libraries
 freeze('/home/user/sources/freezeFS/', 'freezefs_mount.py') # Built-in apps
