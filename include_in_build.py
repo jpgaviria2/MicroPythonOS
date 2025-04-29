8591ba32f1d0c312295faabc60628ccf0160185b
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-04-29 17:10:48 +0200

    Cleanups

--- include_in_build.py
+++
@@ -1,2 +0,0 @@
-print("This script will be included in the build.")
-print("You can then run it with: import include_in_build")
--- manifest.py
+++ manifest.py
@@ -1,4 +1,6 @@
 freeze('internal_filesystem/', 'boot.py') # Hardware initialization
 freeze('internal_filesystem/', 'main.py') # User Interface initialization
+#freeze('', 'boot.py') # Hardware initialization
+#freeze('', 'main.py') # User Interface initialization
 freeze('internal_filesystem/lib', '') # Additional libraries
-freeze('internal_filesystem/', 'apps_builtin') # Built-in apps
+freeze('/home/user/sources/freezeFS/', 'freezefs_mount.py') # Built-in apps
