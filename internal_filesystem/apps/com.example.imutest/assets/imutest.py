f35cf77de5bd4c458d09663a00951d6265b7da42
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-04-29 11:04:24 +0200

    manifest.py: include boot.py and main.py in build

--- internal_filesystem/apps/com.example.imutest/assets/imutest.py
+++ internal_filesystem/apps/com.example.imutest/assets/imutest.py
@@ -37,7 +37,7 @@ def map_nonlinear(value: float) -> int:
 import time
 while appscreen == lv.screen_active():
     #print(f"""{sensor.temperature=} {sensor.acceleration=} {sensor.gyro=}""")
-    templabel.set_text(f"Temperature: {sensor.temperature:.2f}°C")
+    templabel.set_text(f"IMU chip temperature: {sensor.temperature:.2f}°C")
     ax = sensor.acceleration[0]
     axp = int((ax * 100 + 100)/2)
     ay = sensor.acceleration[1]
--- manifest.py
+++ manifest.py
@@ -1,1 +1,3 @@
 freeze('', 'include_in_build.py')
+freeze('', 'internal_filesystem/boot.py', 'boot.py')
+freeze('', 'internal_filesystem/main.py', 'main.py')
