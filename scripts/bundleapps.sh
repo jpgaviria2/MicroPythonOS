30983bd0f6dc0d2824367bb204748df6a1c6ea0c
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-05-07 13:47:55 +0200

    Also bundle builtin apps

--- scripts/bundleapps.sh
+++ scripts/bundleapps.sh
@@ -1,12 +1,18 @@
-rm -rf appstore_backend/bundled_apps
-mkdir -p appstore_backend/bundled_apps
-ls -1 internal_filesystem/apps | while read appdir; do
-    echo "Bundling $appdir"
-    pushd internal_filesystem/apps/"$appdir"
-    version=$( grep "^Version:" META-INF/MANIFEST.MF | cut -d " " -f 2)
-    mpkname="../../../appstore_backend/bundled_apps/"$appdir"_"$version".mpk"
-    echo "Creating $mpkname"
-    zip -r0 "$mpkname" .
-    cp res/mipmap-mdpi/icon_64x64.png "$mpkname"_icon_64x64.png 
-    popd    
+output=appstore_backend/bundled_apps/
+output=$(readlink -f "$output")
+mkdir -p "$output"
+rm "$output"/*.mpk
+
+for apprepo in internal_filesystem/apps internal_filesystem/builtin/apps; do
+	echo "Listing apps in $apprepo"
+	ls -1 "$apprepo" | while read appdir; do
+	    echo "Bundling $apprepo/$appdir"
+	    pushd "$apprepo"/"$appdir"
+	    version=$( grep "^Version:" META-INF/MANIFEST.MF | cut -d " " -f 2)
+	    mpkname="$output"/"$appdir"_"$version".mpk
+	    echo "Creating $mpkname"
+	    zip -r0 "$mpkname" .
+	    cp res/mipmap-mdpi/icon_64x64.png "$mpkname"_icon_64x64.png
+	    popd
+	done
 done
