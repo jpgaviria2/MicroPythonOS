3676fa28daf246996ad4a78b6030c79bb505fc3e
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-05-07 10:38:40 +0200

    scripts/install.sh: add oneapp mode

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,27 @@
 pkill -f "python.*mpremote"
 
+appname="$1"
+
 pushd internal_filesystem/
+
+if [ ! -z "$appname" ]; then
+	echo "Installing one app: $appname"
+	appdir="apps/com.example.$appname/"
+	if [ ! -d "$appdir" ]; then
+		echo "$appdir doesn't exist so taking the builtin/"
+		appdir="builtin/apps/com.example.$appname/"
+		if [ ! -d "$appdir" ]; then
+			echo "$appdir also doesn't exist, exiting..."
+			exit 1
+		fi
+	fi
+	~/sources/lvgl_micropython/lib/micropython/tools/mpremote/mpremote.py fs cp -r "$appdir" :/apps/
+	echo "start_app(\"/$appdir\")"
+	~/sources/lvgl_micropython/lib/micropython/tools/mpremote/mpremote.py
+	popd
+	exit
+fi
+
 
 ~/sources/lvgl_micropython/lib/micropython/tools/mpremote/mpremote.py fs cp boot.py :/boot.py
 ~/sources/lvgl_micropython/lib/micropython/tools/mpremote/mpremote.py fs cp main.py :/main.py
