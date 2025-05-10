40dd2b0b6ef2bed417662c9bafdf627561813b96
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-05-10 17:53:11 +0200

    desktop: fix color format

--- internal_filesystem/boot_unix.py
+++ internal_filesystem/boot_unix.py
@@ -8,9 +8,9 @@ TFT_VER_RES=240
 
 bus = lcd_bus.SDLBus(flags=0)
 
-buf1 = bus.allocate_framebuffer(TFT_HOR_RES * TFT_VER_RES * 3, 0)
+buf1 = bus.allocate_framebuffer(TFT_HOR_RES * TFT_VER_RES * 2, 0)
 
-display = sdl_display.SDLDisplay(data_bus=bus,display_width=TFT_HOR_RES,display_height=TFT_VER_RES,frame_buffer1=buf1,color_space=lv.COLOR_FORMAT.RGB888)
+display = sdl_display.SDLDisplay(data_bus=bus,display_width=TFT_HOR_RES,display_height=TFT_VER_RES,frame_buffer1=buf1,color_space=lv.COLOR_FORMAT.RGB565)
 display.init()
 
 mouse = sdl_pointer.SDLPointer()
