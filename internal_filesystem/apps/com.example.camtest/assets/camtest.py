6181a0898411e647ec3b7542a944bbec9a209ef0
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-05-11 23:38:30 +0200

    camtest: cleanup

--- internal_filesystem/apps/com.example.camtest/assets/camtest.py
+++ internal_filesystem/apps/com.example.camtest/assets/camtest.py
@@ -1,44 +1,5 @@
 appscreen = lv.screen_active()
 
-#define PWDN_GPIO_NUM 17  //power down is not used
-#define RESET_GPIO_NUM -1 //software reset will be performed
-#define XCLK_GPIO_NUM 8
-#define SIOD_GPIO_NUM 21
-#define SIOC_GPIO_NUM 16
-
-#define Y9_GPIO_NUM 2
-#define Y8_GPIO_NUM 7
-#define Y7_GPIO_NUM 10
-#define Y6_GPIO_NUM 14
-#define Y5_GPIO_NUM 11
-#define Y4_GPIO_NUM 15
-#define Y3_GPIO_NUM 13
-#define Y2_GPIO_NUM 12
-#define VSYNC_GPIO_NUM 6
-#define HREF_GPIO_NUM 4
-#define PCLK_GPIO_NUM 9
-
-#define CAM_LEDC_TIMER      LEDC_TIMER_1
-#define CAM_LEDC_CHANNEL    LEDC_CHANNEL_0
-
-
-# OV5640 I2C address
-#sensor_addr = 0x3C
-#reg_addr = 0x4300
-#reg_value = 0x6F  # RGB565 (bit[6]=1) + sequence 0xF (bits[3:0]=0xF)
-#from machine import I2C, Pin
-#i2c = I2C(scl=Pin(16), sda=Pin(21))
-#i2c.readfrom_mem(0x3C, 0x4300, 1)
-#i2c.writeto_mem(sensor_addr, reg_addr, bytes([reg_value]))
-
-
-#width = 480
-#height = 320
-#width = 320
-#height = 240
-#width = 120
-#height = 160
-
 keepgoing = True
 width = 240
 height = 240
