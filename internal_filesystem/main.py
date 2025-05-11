c09e023cca9fe63ff7e04d7ce7e1ef88e44b66fe
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-05-11 23:56:28 +0200

    main.py: simplify

--- internal_filesystem/main.py
+++ internal_filesystem/main.py
@@ -1,11 +1,7 @@
-import lvgl as lv
 import task_handler
-
-# 5ms is recommended for MicroPython+LVGL on desktop:
-th = task_handler.TaskHandler(duration=5)
+th = task_handler.TaskHandler(duration=5) # 5ms is recommended for MicroPython+LVGL on desktop
 
 from mpos import ui
-
 ui.create_rootscreen()
 ui.create_notification_bar()
 ui.create_drawer(display)
@@ -16,7 +12,6 @@ except Exception as e:
     print("main.py: WARNING: could not import/run freezefs_mount_builtin: ", e)
 
 from mpos import apps
-
 apps.execute_script_new_thread("builtin/system/button.py", True, False, False) # Button handling through IRQ
 apps.auto_connect()
 apps.restart_launcher()
