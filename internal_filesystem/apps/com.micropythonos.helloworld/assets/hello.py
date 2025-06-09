415b3d7eb380df543217ef12002a6a58bc8f7ccb
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-06-09 16:00:38 +0200

    Add HelloWorld app

---
+++ internal_filesystem/apps/com.micropythonos.helloworld/assets/hello.py
@@ -0,0 +1,10 @@
+from mpos.apps import Activity
+
+class Hello(Activity):
+
+    def onCreate(self):
+        screen = lv.obj()
+        label = lv.label(screen)
+        label.set_text('Hello World!')
+        label.center()
+        self.setContentView(screen)
