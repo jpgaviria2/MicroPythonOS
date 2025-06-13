fdd42d8914c2f018aadfbcc143b672d5ab012a95
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-06-13 09:52:08 +0200

    add some patches

---
+++ patches/esp32_components.cmake
@@ -0,0 +1,5 @@
+
+
+include(/home/user/sources/micropython-camera-API/src/micropython.cmake)
+include(/home/user/sources/MicroPythonOS/c_mpos/secp256k1-embedded_kdmukai/micropython.cmake)
+include(/home/user/sources/MicroPythonOS/c_mpos/micropython.cmake)
