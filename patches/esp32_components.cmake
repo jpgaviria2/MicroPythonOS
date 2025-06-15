af37bf2810729612a4f4f1bc75c165953b382df6
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-06-15 10:18:41 +0200

    Remove unused files

--- patches/esp32_components.cmake
+++
@@ -1,5 +0,0 @@
-
-
-include(/home/user/sources/micropython-camera-API/src/micropython.cmake)
-include(/home/user/sources/MicroPythonOS/c_mpos/secp256k1-embedded_kdmukai/micropython.cmake)
-include(/home/user/sources/MicroPythonOS/c_mpos/micropython.cmake)
--- scripts/pngtobin.sh
+++
@@ -1,3 +0,0 @@
-#~/sources/lvgl_micropython/lib/lvgl/scripts/LVGLImage.py --ofmt BIN --cf RGB565 -o icon_64x64.bin icon_64x64.png 
-#~/sources/lvgl_micropython/lib/lvgl/scripts/LVGLImage.py --ofmt BIN --cf RGB565 icon_64x64.png 
-~/sources/lvgl_micropython/lib/lvgl/scripts/LVGLImage.py --ofmt BIN --cf RGB565A8 "$1"
